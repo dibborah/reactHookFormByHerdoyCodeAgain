506313d4cae998fa055ac3ddfd7183ec00bc684a
dibya.borah <dibyajyotiborah469@gmail.com>
2024-05-28 01:38:57 +0530

    completed joi validation by herdoy using react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { joiResolver } from '@hookform/resolvers/joi'
+import Joi from "joi";
 
-type authFormFields = {
+interface authFormFields {
   email: string,
-  password: String,
+  password: string,
 }
 
+const schema = Joi.object({
+  email: Joi.string().min(5).max(200).required().label('Email'),
+  password: Joi.string().min(8).max(100).required().label('Password')
+})
+ 
 const App = () => {
-  const formMethod = useForm();
+  const formMethod = useForm<authFormFields>({
+    resolver: joiResolver(schema)
+  });
+  const onSubmit:SubmitHandler<authFormFields> = (data: any) => {
+    console.log(data);
+  }
   return (
     <div className="container my-5">
-      <form>
+      <form onSubmit={formMethod.handleSubmit(onSubmit)}>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="email" />
+          <input {...formMethod.register('email')} type="email" className="form-control" id="email" />
+          {formMethod?.formState?.errors?.email && <div className="text-danger"> {formMethod.formState.errors.email.message}</div> }
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" id="password" />
+          <input {...formMethod.register('password')} type="password" className="form-control" id="password" />
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+          {formMethod?.formState?.errors?.password && <div className="text-danger"> {formMethod.formState.errors.password.message}</div> }
+        <button disabled={!formMethod.formState.isValid} type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
   )
